36c3f2d640a9f1296bebae3bae255bab103007fb
林彬强 <364893081@qq.com>
2021-02-06 11:48:19 +0800

    首页布局
    首页布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    首页
+  <div class="pad-15">
+    <!-- 库存数据 -->
+    <div class="flex flex-around pad-10 data-info-box">
+      <div class="align-c">
+        <span>8888</span>
+        <span class="mar-10">总库存</span>
+      </div>
+      <div class="align-c">
+        <span>100</span>
+        <span class="mar-10">本月新入库</span>
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -11,5 +22,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.data-info-box {
+  background: #fff;
+  box-shadow: 0rpx 4rpx 6rpx 4rpx #f2f2f2;
+}
 </style>
