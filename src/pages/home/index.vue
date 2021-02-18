92343a873a11d1c7321d95c3a68dad12a789593e
林彬强 <364893081@qq.com>
2021-02-18 13:59:59 +0800

    提交
    提交

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -11,13 +11,39 @@
         <span class="mar-10 font-color-9">本月新入库</span>
       </div>
     </div>
-
+    <!-- 库存管理 -->
+    <div class="mar-t-15">
+      <span class="font-w">
+        库存管理
+      </span>
+      <div class="mar-t-15 flex  flex-wrap">
+        <div class="flex-col" @click="goAddGoods">
+          <div class="btn-bg width-48 height-48 flex flex-center">
+            <img class="width-26 height-26" src="../../img/warehousing.png" alt="">
+          </div>
+          <span class="font-12 mar-t-6 font-color-9">采购入库</span>
+        </div>
+        <div class="flex-col mar-l-15">
+          <div class="btn-bg width-48 height-48 flex flex-center">
+            <img class="width-26 height-26" src="../../img/out-of-stock.png" alt="">
+          </div>
+          <span class="font-12 mar-t-6 font-color-9">商品出库</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {}
+  },
+  methods: {
+    goAddGoods () {
+      wx.navigateTo({url: '/pages/warehousing/main'})
+    }
+  }
 }
 </script>
 
@@ -25,5 +51,8 @@ export default {
 .data-info-box {
   background: #fff;
   box-shadow: 0rpx 4rpx 6rpx 4rpx #f2f2f2;
+}
+.btn-bg {
+  background: #f5f5f5;
 }
 </style>
---
+++ src/pages/warehousing/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="pad-t-15">
+    <div class="panel panel-box">
+      <div class="flex v-align-c div-input mar-r-20">
+        <div class="mar-r-10 width-80 font-color-3 font-15">商品名称</div>
+        <div class="flex-item"><input  class="font-black" placeholder="请输入商品名称"/></div>
+      </div>
+      <div class="flex v-align-c div-input mar-r-20">
+        <div class="mar-r-10 width-80 font-color-3 font-15">商品编码</div>
+        <div class="flex-item"><input class="font-black" placeholder="请输入商品编码"/></div>
+      </div>
+      <div class="flex v-align-c div-input mar-r-20">
+        <div class="mar-r-10 width-80 font-color-3 font-15">商品数量</div>
+        <div class="flex-item pad-t-4 pad-b-4">
+        </div>
+      </div>
+      <div class="flex v-align-c div-input mar-r-20">
+        <div class="mar-r-10 width-80 font-color-3 font-15">采购金额</div>
+        <div class="flex-item"><input class="font-black" placeholder="请输入采购金额"/></div>
+      </div>
+    </div>
+    <div class="mar-15 pad-t-10">
+      <button size="default" class="primary-button font-w">添加入库</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .div-input {
+    border-bottom: 0.5px solid #ebebeb;
+    margin-left: 20px;
+    padding-top: 11px;
+    padding-bottom: 11px;
+    font-size: 13px;
+  }
+  .div-input:last-child {
+    border-bottom: 0;
+  }
+  .primary-button{
+    border-radius: 100px;
+    background: #7427FF;
+    color: #fff;
+  }
+</style>
